fa382f8ea6f03a0247febe555aee64cbf0859bc6
ZakharBessonov <zakhar200702@gmail.com>
2025-10-18 23:57:24 +0300

    Deleted files

--- consts.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTS_HEADER
-#define CONSTS_HEADER
-
-const size_t MAXSIZE = 50;
-const size_t MAX_SIZE_OF_COMMAND = 8;
-const size_t MAX_CNT_OF_LABELS = 10;
-const char LOG_FILE_NAME[] = "logfile.txt";
-
-#endif
--- registers.h
+++
@@ -1,9 +0,0 @@
-#ifndef REGISTERS_HEADER
-#define REGISTERS_HEADER
-
-const size_t numOfRegisters = 8;
-const size_t lengthOfNameForRegister = 3;
-
-const char registers[numOfRegisters][lengthOfNameForRegister] = {"AX", "BX", "CX", "DX", "EX", "FX", "GX", "HX"};
-
-#endif
